26460a57a1418481bb68e22092fe9b3a9d532c51
autobzooty <autobzooty@gmail.com>
2020-11-28 18:17:34 -0800

    fixed project name

---
+++ SuperAdventure3D/Assets/Star.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+using UnityEngine.SceneManagement;
+
+public class Star : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+    Tween.LocalPosition(this.transform, this.transform.position + new Vector3(0, -0.25f, 0), 0.75f, 0, Tween.EaseInOut, Tween.LoopType.PingPong);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+        
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    GameObject obj = other.gameObject;
+    if(obj.GetComponent<PlayerMover>())
+    {
+      int scene = SceneManager.GetActiveScene().buildIndex;
+      SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+  }
+}
