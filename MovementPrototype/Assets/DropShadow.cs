6f70487136a06a5400351f8fd93e1ee828d8eb98
Alek <autobzooty@gmail.com>
2021-11-28 17:15:39 -0800

    Added a drop shadow component

---
+++ MovementPrototype/Assets/DropShadow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadow : MonoBehaviour
+{
+  public GameObject DropShadowPrefab;
+  private GameObject MyDropShadow;
+  void Start()
+  {
+    MyDropShadow = GameObject.Instantiate(DropShadowPrefab);
+  }
+
+  void Update()
+  {
+    Ray ray = new Ray(transform.position + Vector3.up, Vector3.down);
+    float maxDistance = 20.0f;
+    int layerMask = LayerMask.GetMask("Default");
+
+    Physics.Raycast(ray, out RaycastHit hit, maxDistance, layerMask);
+    MyDropShadow.transform.position = hit.point + (Vector3.up * 0.01f);
+  }
+
+  private void OnDestroy()
+  {
+    Destroy(MyDropShadow);
+  }
+}
