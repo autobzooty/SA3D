2a79db4fba13032b52a80dde035abadac495c3fe
Doug Zwick <modusponens@gmail.com>
2024-04-19 15:26:18 -0700

    Updated .gitignore and cleaned up the incorrectly tracked files

--- SuperAdventure3D/Intermediate/Build/Win64/x64/SuperAdventure3DEditor/Development/Core/SharedPCH.Core.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/MilkyWayGames/Super Adventure 3D/SA3D/SuperAdventure3D/Intermediate/Build/Win64/x64/SuperAdventure3DEditor/Development/Core/SharedDefinitions.Core.Cpp17.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- SuperAdventure3D/Intermediate/Build/Win64/x64/SuperAdventure3DEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.39.33523
-#include "C:/MilkyWayGames/Super Adventure 3D/SA3D/SuperAdventure3D/Intermediate/Build/Win64/x64/SuperAdventure3DEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
