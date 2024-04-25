d4d7a5ce782107d46a3bad994d4a198704a9c6b9
Alek Hiebert <aleknhiebert@gmail.com>
2024-04-25 02:27:29 -0700

    Set up state machine frame work.  Added camera controls and turning logic for the MilkyWayPawn. Need to fix the jittering on turning.

--- SuperAdventure3D/Source/SuperAdventure3D/Private/MilkyWayPawn.cpp
+++ SuperAdventure3D/Source/SuperAdventure3D/Private/MilkyWayPawn.cpp
@@ -2,13 +2,39 @@
 
 
 #include "MilkyWayPawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
 
 // Sets default values
 AMilkyWayPawn::AMilkyWayPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Instantiating your class Components
+	RootArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("RootArrow"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+
+	RootArrow->SetupAttachment(GetRootComponent());
+
+	//Attach the Spring Arm to the root
+	SpringArm->SetupAttachment(RootArrow);
+
+	//Attach the Camera to the SpringArmComponent
+	Camera->AttachToComponent(SpringArm, FAttachmentTransformRules::KeepRelativeTransform);
+
+	//Setting default properties of the SpringArmComp
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->TargetArmLength = 1000.0f;	
 }
 
 // Called when the game starts or when spawned
@@ -24,26 +50,166 @@ void AMilkyWayPawn::Tick(float _DeltaTime)
 	Super::Tick(_DeltaTime);
 	DeltaTime = _DeltaTime;
 
+	switch (CurrentState)
+	{
+		case Idle:
+			Idle_Tick();
+			break;
+		case Walk:
+			Walk_Tick();
+			break;
+		case Stop:
+			Stop_Tick();
+			break;
+		case Jump:
+			Jump_Tick();
+			break;
+	}
 }
 
 // Called to bind functionality to input
 void AMilkyWayPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	PlayerInputComponent->BindAxis(TEXT("StickForward"), this, &AMilkyWayPawn::MoveForward);
-	PlayerInputComponent->BindAxis(TEXT("StickRight"), this, &AMilkyWayPawn::MoveRight);
+
+	PlayerInputComponent->BindAxis(TEXT("LeftStickVertical"), this, &AMilkyWayPawn::OnLeftStickVertical);
+	PlayerInputComponent->BindAxis(TEXT("LeftStickHorizontal"), this, &AMilkyWayPawn::OnLeftStickHorizontal);
+	PlayerInputComponent->BindAxis(TEXT("RightStickVertical"), this, &AMilkyWayPawn::OnRightStickVertical);
+	PlayerInputComponent->BindAxis(TEXT("RightStickHorizontal"), this, &AMilkyWayPawn::OnRightStickHorizontal);
+
+	//Add input mapping context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		//if(UEnhancedInputLocalPlayerSubsystem* Subsystem = )
+	}
 
 }
 
-void AMilkyWayPawn::MoveForward(float AxisValue)
+void AMilkyWayPawn::OnLeftStickVertical(float AxisValue)
 {
-	FVector newLocation = GetActorLocation() + GetActorForwardVector() * AxisValue * Speed * DeltaTime;
+	CurrentLeftStick.Y = AxisValue;
+	CurrentLeftStick.Normalize();
+}
+
+void AMilkyWayPawn::OnLeftStickHorizontal(float AxisValue)
+{
+	CurrentLeftStick.X = AxisValue;
+	CurrentLeftStick.Normalize();
+}
+
+void AMilkyWayPawn::OnRightStickVertical(float AxisValue)
+{
+	if (GEngine)
+	{
+		//Print debug message
+		GEngine->AddOnScreenDebugMessage(-10, 1.f, FColor::Yellow, "Hello");
+	}
+
+	CurrentRightStick.Y = AxisValue;
+	CurrentRightStick.Normalize();
+
+	float newPitch = CameraTurnSpeed * AxisValue * DeltaTime;
+	FRotator rotator = FRotator(newPitch, 0, 0);
+
+	SpringArm->AddLocalRotation(rotator);
+}
+
+void AMilkyWayPawn::OnRightStickHorizontal(float AxisValue)
+{
+	
+	CurrentRightStick.X = AxisValue;
+	CurrentRightStick.Normalize();
+
+	float newYaw = CameraTurnSpeed * AxisValue * DeltaTime;
+	FRotator rotator = FRotator(0, newYaw, 0);
+
+	SpringArm->AddWorldRotation(rotator);
+}
+
+void AMilkyWayPawn::Idle_Tick()
+{
+	if (CurrentLeftStick.Length() > 0)
+	{
+		//Snap rotation to camera's requested move direction
+		FRotator rotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), GetActorLocation() + GetCameraRequestedMoveDirection());
+		SetActorRotation(rotation);
+
+		CurrentState = Walk;
+	}
+}
+
+void AMilkyWayPawn::Walk_Tick()
+{
+	if (CurrentLeftStick.Length() == 0)
+	{
+		CurrentState = Stop;
+	}
+
+	//Accelerate in facing direction
+	if (HSpeed < BaseMaxGroundSpeed)
+	{
+		HSpeed += CurrentLeftStick.Length() * GroundAcceleration * DeltaTime;
+	}
+
+	//Rotate toward camera's requested direction
+	FVector cameraRequestedMoveDirection = GetCameraRequestedMoveDirection();
+	float turnDirection = 1;
+	if (cameraRequestedMoveDirection.Dot(GetActorRightVector()) < 0)
+	{
+		turnDirection = -1;
+	}
+	float turnAmount = TurnSpeed * turnDirection * DeltaTime;
+	FRotator rotator = FRotator(0, turnAmount, 0);
+	AddActorLocalRotation(rotator);
+
+	Move();
+}
+
+void AMilkyWayPawn::Stop_Tick()
+{
+	if (CurrentLeftStick.Length() > 0)
+	{
+		CurrentState = Walk;
+	}
+	if (HSpeed == 0)
+	{
+		CurrentState = Idle;
+	}
+	else if (HSpeed > 0)
+	{
+		HSpeed -= GroundDeceleration * DeltaTime;
+	}
+	else if (HSpeed < 0)
+	{
+		HSpeed += GroundDeceleration * DeltaTime;
+	}
+	if (abs(HSpeed) < GroundDeceleration * DeltaTime)
+	{
+		HSpeed = 0;
+	}
+	Move();
+}
+
+void AMilkyWayPawn::Jump_Tick()
+{
+	
+}
+
+void AMilkyWayPawn::Move()
+{
+	FVector newLocation = GetActorLocation() + GetActorForwardVector() * HSpeed;
 	SetActorLocation(newLocation);
 }
 
-void AMilkyWayPawn::MoveRight(float AxisValue)
+FVector AMilkyWayPawn::GetCameraRequestedMoveDirection()
 {
-	FVector newLocation = GetActorLocation() + GetActorRightVector() * AxisValue * Speed * DeltaTime;
-	SetActorLocation(newLocation);
+	FVector leftStick = FVector(CurrentLeftStick.Y, CurrentLeftStick.X, 0);
+	if (leftStick.Length() == 0)
+	{
+		return GetActorForwardVector();
+	}
+	FVector cameraRequestedMoveDirection = UKismetMathLibrary::TransformDirection(Camera->GetComponentTransform(), leftStick);
+	cameraRequestedMoveDirection.Z = 0;
+	cameraRequestedMoveDirection.Normalize();
+	return cameraRequestedMoveDirection;
 }
-
--- SuperAdventure3D/Source/SuperAdventure3D/Private/MilkyWayPawn.h
+++ SuperAdventure3D/Source/SuperAdventure3D/Private/MilkyWayPawn.h
@@ -14,10 +14,20 @@ class AMilkyWayPawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	AMilkyWayPawn();
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UArrowComponent* RootArrow;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class USpringArmComponent* SpringArm;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UCameraComponent* Camera;
+	
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	
 
 public:	
 	// Called every frame
@@ -27,13 +37,40 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(EditAnywhere)
-	float Speed;
+	float GroundAcceleration = 500;
+
+	UPROPERTY(EditAnywhere)
+	float GroundDeceleration = 1000;
+
+	UPROPERTY(EditAnywhere)
+	float TurnSpeed = 540;
+
+	UPROPERTY(EditAnywhere)
+	float BaseMaxGroundSpeed = 500;
+
+	UPROPERTY(EditAnywhere)
+	float CameraTurnSpeed = 180;
 
 protected:
 
-	void MoveForward(float AxisValue);
-	void MoveRight(float AxisValue);
+	void OnLeftStickVertical(float AxisValue);
+	void OnLeftStickHorizontal(float AxisValue);
+	void OnRightStickVertical(float AxisValue);
+	void OnRightStickHorizontal(float AxisValue);
+	void Idle_Tick();
+	void Walk_Tick();
+	void Stop_Tick();
+	void Jump_Tick();
+	void Move();
+	FVector GetCameraRequestedMoveDirection();
 
 private:
 	float DeltaTime;
+	enum PawnStates {Idle , Walk , Stop , Jump};
+	PawnStates CurrentState = Idle;
+	FVector2D CurrentLeftStick;
+	FVector2D CurrentRightStick;
+	float HSpeed;
+	float VSpeed;
+
 };
